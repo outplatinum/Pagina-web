1e72c048ac7b907d79e2877f8821712a90be4e69
outplatinum <vinicius.damasio@escola.pr.gov.br>
2026-07-02 10:59:28 -0300

    perguntas programação

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Esporte',
+    'Programação',
     'O que é uma linguagem de programação orientada a objetos?',
     'É uma linguagem baseada no conceito de objetos, que combinam dados (atributos) e comportamentos (métodos). Exemplos incluem Java, C# e Python.'
 )
 
 criaCartao(
-    'Geografia',
+    'Programação',
     'O que é um algoritmo?',
     'Um algoritmo é uma sequência lógica de passos para resolver um problema ou realizar uma tarefa.'
 )
 
 criaCartao(
-    'Astronomia',
+    'Programação',
     'O que é uma variável?',
     'Uma variável é um espaço na memória usado para armazenar um valor que pode ser alterado durante a execução do programa.'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'Programação inglesa',
     'O que é uma função?',
     'Uma função é um bloco de código reutilizável que executa uma tarefa específica e pode receber parâmetros e retornar um valor.)'
 )
